f0aaed338861dcb9cac6e96e8d05ddfc8e996c64
Antony Siegert <antony.siegert@infopark.de>
2017-06-28 10:26:30 +0200

    encapsulation with model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,52 @@
 class ApplicationController < ActionController::Base
+  def hello_world
+    name = params['name'] || 'World'
+    render 'application/hello_world', locals: { name: name }
+  end
+
+  def list_posts
+    posts = Post.all
+
+
+    render 'application/list_posts', locals: { posts: posts }
+  end
+
+  def show_post
+    post = Post.find(params['id'])
+
+    render 'application/show_post', locals: { post: post }
+  end
+
+  def create_post
+    post = Post.new('title' => params['title'],
+                    'body' => params['body'],
+                    'author' => params['author'])
+    post.save
+    redirect_to '/list_posts'
+  end
+
+  def edit_post
+    post = Post.find(params['id'])
+
+    render 'application/edit_post', locals: { post: post }
+  end
+
+  def update_post
+    update_query = <<-SQL
+      UPDATE posts
+      SET title      = ?,
+          body       = ?,
+          author     = ?
+      WHERE posts.id = ?
+    SQL
+    connection.execute update_query, params['title'], params['body'], params['author'], params['id']
+
+    redirect_to '/list_posts'
+  end
+
+  def delete_post
+    connection.execute("DELETE FROM posts WHERE posts.id = ?", params['id'])
+
+    redirect_to '/list_posts'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get '/hello_world' => 'application#hello_world'
+  get '/hello/:name' => 'application#hello_world'
+
+  get '/list_posts' => 'application#list_posts'
+  get 'show_post/:id' => 'application#show_post'
+
+  get '/new_post' => 'application#new_post'
+  post '/create_post' => 'application#create_post'
+
+  get 'edit_post/:id' => 'application#edit_post'
+  post '/update_post/:id' => 'application#update_post'
+
+  post 'delete_post/:id' => 'application#delete_post'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,7 +20,6 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
